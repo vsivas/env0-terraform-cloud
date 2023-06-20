5f9dad84b5ee1693061784b8bdfad4dd3da50a42
Sam Gabrail <sam.gabrail@tekanaid.com>
2023-06-20 13:00:03 -0400

    reduce cost

--- .infracost/terraform_modules/319e2fa69a1a47d629579e9bb0cfad96/examples/eks_managed_node_group/main.tf
+++ .infracost/terraform_modules/319e2fa69a1a47d629579e9bb0cfad96/examples/eks_managed_node_group/main.tf
@@ -37,44 +37,43 @@ locals {
 ################################################################################
 
 module "eks" {
-  source  = "app.terraform.io/TeKanAid/eks/aws"
-  version = "0.0.1"
+  source = "../.."
 
   cluster_name                   = local.name
   cluster_version                = local.cluster_version
   cluster_endpoint_public_access = true
 
   # IPV6
-#   cluster_ip_family = "ipv6"
+  cluster_ip_family = "ipv6"
 
-#   # We are using the IRSA created below for permissions
-#   # However, we have to deploy with the policy attached FIRST (when creating a fresh cluster)
-#   # and then turn this off after the cluster/node group is created. Without this initial policy,
-#   # the VPC CNI fails to assign IPs and nodes cannot join the cluster
-#   # See https://github.com/aws/containers-roadmap/issues/1666 for more context
-#   # TODO - remove this policy once AWS releases a managed version similar to AmazonEKS_CNI_Policy (IPv4)
-#   create_cni_ipv6_iam_policy = true
+  # We are using the IRSA created below for permissions
+  # However, we have to deploy with the policy attached FIRST (when creating a fresh cluster)
+  # and then turn this off after the cluster/node group is created. Without this initial policy,
+  # the VPC CNI fails to assign IPs and nodes cannot join the cluster
+  # See https://github.com/aws/containers-roadmap/issues/1666 for more context
+  # TODO - remove this policy once AWS releases a managed version similar to AmazonEKS_CNI_Policy (IPv4)
+  create_cni_ipv6_iam_policy = true
 
-#   cluster_addons = {
-#     coredns = {
-#       most_recent = true
-#     }
-#     kube-proxy = {
-#       most_recent = true
-#     }
-#     vpc-cni = {
-#       most_recent              = true
-#       before_compute           = true
-#       service_account_role_arn = module.vpc_cni_irsa.iam_role_arn
-#       configuration_values = jsonencode({
-#         env = {
-#           # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
-#           ENABLE_PREFIX_DELEGATION = "true"
-#           WARM_PREFIX_TARGET       = "1"
-#         }
-#       })
-#     }
-#   }
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent              = true
+      before_compute           = true
+      service_account_role_arn = module.vpc_cni_irsa.iam_role_arn
+      configuration_values = jsonencode({
+        env = {
+          # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
+          ENABLE_PREFIX_DELEGATION = "true"
+          WARM_PREFIX_TARGET       = "1"
+        }
+      })
+    }
+  }
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
@@ -82,17 +81,17 @@ module "eks" {
 
   manage_aws_auth_configmap = true
 
-#   eks_managed_node_group_defaults = {
-#     ami_type       = "AL2_x86_64"
-#     instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
+  eks_managed_node_group_defaults = {
+    ami_type       = "AL2_x86_64"
+    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
 
-#     # We are using the IRSA created below for permissions
-#     # However, we have to deploy with the policy attached FIRST (when creating a fresh cluster)
-#     # and then turn this off after the cluster/node group is created. Without this initial policy,
-#     # the VPC CNI fails to assign IPs and nodes cannot join the cluster
-#     # See https://github.com/aws/containers-roadmap/issues/1666 for more context
-#     iam_role_attach_cni_policy = true
-#   }
+    # We are using the IRSA created below for permissions
+    # However, we have to deploy with the policy attached FIRST (when creating a fresh cluster)
+    # and then turn this off after the cluster/node group is created. Without this initial policy,
+    # the VPC CNI fails to assign IPs and nodes cannot join the cluster
+    # See https://github.com/aws/containers-roadmap/issues/1666 for more context
+    iam_role_attach_cni_policy = true
+  }
 
   eks_managed_node_groups = {
     # Default node group - as provided by AWS EKS
@@ -110,78 +109,78 @@ module "eks" {
       }
     }
 
-    # # Default node group - as provided by AWS EKS using Bottlerocket
-    # bottlerocket_default = {
-    #   # By default, the module creates a launch template to ensure tags are propagated to instances, etc.,
-    #   # so we need to disable it to use the default template provided by the AWS EKS managed node group service
-    #   use_custom_launch_template = false
+    # Default node group - as provided by AWS EKS using Bottlerocket
+    bottlerocket_default = {
+      # By default, the module creates a launch template to ensure tags are propagated to instances, etc.,
+      # so we need to disable it to use the default template provided by the AWS EKS managed node group service
+      use_custom_launch_template = false
 
-    #   ami_type = "BOTTLEROCKET_x86_64"
-    #   platform = "bottlerocket"
-    # }
+      ami_type = "BOTTLEROCKET_x86_64"
+      platform = "bottlerocket"
+    }
 
-    # # Adds to the AWS provided user data
-    # bottlerocket_add = {
-    #   ami_type = "BOTTLEROCKET_x86_64"
-    #   platform = "bottlerocket"
+    # Adds to the AWS provided user data
+    bottlerocket_add = {
+      ami_type = "BOTTLEROCKET_x86_64"
+      platform = "bottlerocket"
 
-    #   # This will get added to what AWS provides
-    #   bootstrap_extra_args = <<-EOT
-    #     # extra args added
-    #     [settings.kernel]
-    #     lockdown = "integrity"
-    #   EOT
-    # }
+      # This will get added to what AWS provides
+      bootstrap_extra_args = <<-EOT
+        # extra args added
+        [settings.kernel]
+        lockdown = "integrity"
+      EOT
+    }
 
-    # # Custom AMI, using module provided bootstrap data
-    # bottlerocket_custom = {
-    #   # Current bottlerocket AMI
-    #   ami_id   = data.aws_ami.eks_default_bottlerocket.image_id
-    #   platform = "bottlerocket"
+    # Custom AMI, using module provided bootstrap data
+    bottlerocket_custom = {
+      # Current bottlerocket AMI
+      ami_id   = data.aws_ami.eks_default_bottlerocket.image_id
+      platform = "bottlerocket"
 
-    #   # Use module user data template to bootstrap
-    #   enable_bootstrap_user_data = true
-    #   # This will get added to the template
-    #   bootstrap_extra_args = <<-EOT
-    #     # The admin host container provides SSH access and runs with "superpowers".
-    #     # It is disabled by default, but can be disabled explicitly.
-    #     [settings.host-containers.admin]
-    #     enabled = false
+      # Use module user data template to bootstrap
+      enable_bootstrap_user_data = true
+      # This will get added to the template
+      bootstrap_extra_args = <<-EOT
+        # The admin host container provides SSH access and runs with "superpowers".
+        # It is disabled by default, but can be disabled explicitly.
+        [settings.host-containers.admin]
+        enabled = false
 
-    #     # The control host container provides out-of-band access via SSM.
-    #     # It is enabled by default, and can be disabled if you do not expect to use SSM.
-    #     # This could leave you with no way to access the API and change settings on an existing node!
-    #     [settings.host-containers.control]
-    #     enabled = true
+        # The control host container provides out-of-band access via SSM.
+        # It is enabled by default, and can be disabled if you do not expect to use SSM.
+        # This could leave you with no way to access the API and change settings on an existing node!
+        [settings.host-containers.control]
+        enabled = true
 
-    #     # extra args added
-    #     [settings.kernel]
-    #     lockdown = "integrity"
+        # extra args added
+        [settings.kernel]
+        lockdown = "integrity"
 
-    #     [settings.kubernetes.node-labels]
-    #     label1 = "foo"
-    #     label2 = "bar"
+        [settings.kubernetes.node-labels]
+        label1 = "foo"
+        label2 = "bar"
 
-    #     [settings.kubernetes.node-taints]
-    #     dedicated = "experimental:PreferNoSchedule"
-    #     special = "true:NoSchedule"
-    #   EOT
-    # }
+        [settings.kubernetes.node-taints]
+        dedicated = "experimental:PreferNoSchedule"
+        special = "true:NoSchedule"
+      EOT
+    }
 
-    # # Use a custom AMI
-    # custom_ami = {
-    #   ami_type = "AL2_ARM_64"
-    #   # Current default AMI used by managed node groups - pseudo "custom"
-    #   ami_id = data.aws_ami.eks_default_arm.image_id
+    # Use a custom AMI
+    custom_ami = {
+      ami_type = "AL2_ARM_64"
+      # Current default AMI used by managed node groups - pseudo "custom"
+      ami_id = data.aws_ami.eks_default_arm.image_id
 
-    #   # This will ensure the bootstrap user data is used to join the node
-    #   # By default, EKS managed node groups will not append bootstrap script;
-    #   # this adds it back in using the default template provided by the module
-    #   # Note: this assumes the AMI provided is an EKS optimized AMI derivative
-    #   enable_bootstrap_user_data = true
+      # This will ensure the bootstrap user data is used to join the node
+      # By default, EKS managed node groups will not append bootstrap script;
+      # this adds it back in using the default template provided by the module
+      # Note: this assumes the AMI provided is an EKS optimized AMI derivative
+      enable_bootstrap_user_data = true
 
-    #   instance_types = ["t4g.medium"]
-    # }
+      instance_types = ["t4g.medium"]
+    }
 
     # Complete
     complete = {
@@ -191,7 +190,7 @@ module "eks" {
       subnet_ids = module.vpc.private_subnets
 
       min_size     = 1
-      max_size     = 3
+      max_size     = 7
       desired_size = 1
 
       ami_id                     = data.aws_ami.eks_default.image_id
@@ -207,7 +206,7 @@ module "eks" {
 
       capacity_type        = "SPOT"
       force_update_version = true
-      instance_types       = ["t2.micro"]
+      instance_types       = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
       labels = {
         GithubRepo = "terraform-aws-eks"
         GithubOrg  = "terraform-aws-modules"
