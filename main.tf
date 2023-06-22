21571c15ad96d5cf988e0c231fba412d331e059f
Sam Gabrail <sam.gabrail@tekanaid.com>
2023-06-22 09:06:53 -0400

    ran terraform fmt

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "eks" {
   source  = "app.terraform.io/TeKanAid/eks/aws"
   version = "0.0.7"
 
-  region = "us-east-1"
+  region               = "us-east-1"
   cluster_version      = "1.27"
   cluster_name         = "env0_eks_cluster"
   instance_types       = ["t2.small"]
